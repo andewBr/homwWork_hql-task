0671be6a1bcd3bf39d2d47ff63cc44944a21e0b6
Andrew <burov.andrew@gmail.com>
2024-05-30 15:59:52 +0300

    first commit 3

--- src/test/java/by/itacademy/hibernate/dao/ApartmentDaoTest.java
+++ src/test/java/by/itacademy/hibernate/dao/ApartmentDaoTest.java
@@ -1,10 +1,8 @@
 package by.itacademy.hibernate.dao;
 
 import by.itacademy.hibernate.entity.Apartment;
-import by.itacademy.hibernate.entity.User;
 import by.itacademy.hibernate.util.HibernateUtil;
 import by.itacademy.hibernate.utils.TestDataImporter;
-import com.querydsl.jpa.impl.JPAQuery;
 import lombok.Cleanup;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -16,8 +14,6 @@ import org.junit.jupiter.api.TestInstance;
 import java.util.Arrays;
 import java.util.List;
 
-import static by.itacademy.hibernate.entity.QApartment.apartment;
-import static by.itacademy.hibernate.entity.QUser.user;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
@@ -38,31 +34,6 @@ public class ApartmentDaoTest {
     public void finish() {
         sessionFactory.close();
     }
-
-
-    /**
-     * Возвращает все квартиры > указанного размера
-     */
-//    public List<Apartment> getApartmentGtThenSize(Session session, Integer size)
-
-    /**
-     * Возвращает апартаменты которые находятся в диапазоне между minSize maxSize
-     */
-//    public List<Apartment> findApartmentsBySize(Session session, int minSize, int maxSize)
-
-//    @Test
-//    void findAllByFirstName() {
-//        @Cleanup Session session = sessionFactory.openSession();
-//        session.beginTransaction();
-//
-//        List<User> results = userDao.findAllByFirstName(session, "Bill");
-//
-//        assertThat(results).hasSize(1);
-//        assertThat(results.get(0).fullName()).isEqualTo("Bill Gates");
-//
-//        session.getTransaction().commit();
-//    }
-
 
     @Test
     void findAllApplications() {
@@ -100,7 +71,17 @@ public class ApartmentDaoTest {
         session.beginTransaction();
 
         List<String> apartmentsByRange = apartmentDao.findApartmentsByRange(session, 20, 60);
-        System.out.println(apartmentsByRange.equals(Arrays.asList("generala belova 28", "panfilova str 17")));
+        assertThat(apartmentsByRange.equals(Arrays.asList("generala belova 28", "panfilova str 17")));
     }
+
+    @Test
+    void testApartmentGtThenSize() {
+        @Cleanup Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<Apartment> apartmentGtThenSize = apartmentDao.getApartmentGtThenSize(session, 30);
+        assertThat(apartmentGtThenSize.size()).isEqualTo(1);
+    }
+
 
 }
